31f6934d632c56af367659aea46fa2e84bed4e64
sarseniy <senokosov.ao@phystech.edu>
2021-11-24 16:47:26 +0300

    WordOpposite added

---
+++ WordOpposite/WordOpposite.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+class Word {
+public:
+    Word() : Word("")
+    {}
+
+    Word(std::string a) : data(a)
+    {}
+    
+    bool operator<(Word const& rha) const {
+        return data < rha.data;
+    }
+
+    void print() const {
+        std::cout << data;
+    }
+
+    friend std::ostream& operator<< (std::ostream& out, const Word& word)
+    {
+        out << word.data;
+
+        return out;
+    }
+private:
+    std::string data;
+};
+
+class Opposite {
+public:
+    Opposite() : Opposite("")
+    {}
+
+    Opposite(std::string a) : data(a)
+    {}
+
+    void print() {
+        std::cout << data;
+    }
+
+    friend std::ostream& operator<< (std::ostream& out, const Opposite& word)
+    {
+        out << word.data;
+
+        return out;
+    }
+private:
+    std::string data;
+};
+
+
+
+int main()
+{
+    std::map<Word, Opposite> m;
+    m.emplace("black", "white");
+    m.emplace("white", "black");
+
+    m.emplace("cold", "hot");
+    m.emplace("hot", "cold");
+    for (auto it = m.begin(); it != m.end(); it++) {
+        std::cout << it->first << ' ' << it->second << '\n';
+    }
+    return 0;
+}
